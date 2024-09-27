64fca8553026654c9670fffcc79dcd30b48f15db
hezbims <hezbisulaiman@gmail.com>
2024-09-27 12:03:51 +0800

    feat: sekarang PPN bisa 0%

--- app/src/main/java/com/hezapp/ekonomis/add_or_update_transaction/domain/use_case/input_form_manipulation/GetValidatedPpnFromInputStringUseCase.kt
+++ app/src/main/java/com/hezapp/ekonomis/add_or_update_transaction/domain/use_case/input_form_manipulation/GetValidatedPpnFromInputStringUseCase.kt
@@ -3,7 +3,7 @@ package com.hezapp.ekonomis.add_or_update_transaction.domain.use_case.input_form
 class GetValidatedPpnFromInputStringUseCase {
     operator fun invoke(input : String) : Int {
         val intValue = input.toInt()
-        if (intValue < 1 || intValue > 100)
+        if (intValue < 0 || intValue > 100)
             throw IllegalArgumentException("Invalid PPN Value")
         return intValue
     }

5d5e16181ea7433374b37b16a9fad93cecb7c4e1
hezbims <hezbisulaiman@gmail.com>
2024-10-01 21:12:38 +0800

    build: naikin versi aplikasi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.hezapp.ekonomis"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

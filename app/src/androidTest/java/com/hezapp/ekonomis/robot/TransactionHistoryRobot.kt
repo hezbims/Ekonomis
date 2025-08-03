aafb6066d9f85b874bbefec7c9904bb965366859
Hezbi Sulaiman <hezbisulaiman@gmail.com>
2025-08-03 10:47:39 +0700

    refactor(TransactionHistory) : buat agar tampilan transaction history bisa di preview karena enggak memerlukan koin dependency

--- app/src/androidTest/java/com/hezapp/ekonomis/robot/TransactionHistoryRobot.kt
+++ app/src/androidTest/java/com/hezapp/ekonomis/robot/TransactionHistoryRobot.kt
@@ -1,5 +1,7 @@
 package com.hezapp.ekonomis.robot
 
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
@@ -7,6 +9,10 @@ import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.hezapp.ekonomis.MainActivity
 import com.hezapp.ekonomis.R
+import com.hezapp.ekonomis.core.presentation.utils.toRupiah
+import com.hezapp.ekonomis.test_data.testCalendarProvider
+import java.time.LocalDate
+import java.util.Calendar
 
 class TransactionHistoryRobot(
     private val composeRule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
@@ -24,5 +30,26 @@ class TransactionHistoryRobot(
         ).performClick()
     }
 
+    @OptIn(ExperimentalTestApi::class)
+    fun clickTransactionCard(
+        profileName: String,
+        totalPrice: Int,
+        date: LocalDate
+    ){
+        val targetSemanticMatcher = hasText(profileName) and
+                hasText(totalPrice.toRupiah()) and
+                hasText(testCalendarProvider.toEddMMMyyyy(testCalendarProvider.getCalendar().apply {
+                    set(Calendar.YEAR, date.year)
+                    set(Calendar.MONTH, date.monthValue - 1)
+                    set(Calendar.DAY_OF_MONTH, date.dayOfMonth)
+                }.timeInMillis))
 
+        composeRule.waitUntilExactlyOneExists(targetSemanticMatcher)
+        composeRule.onNode(targetSemanticMatcher).performClick()
+    }
+
+    fun openAndApplyFilter(month: Int, year: Int){
+        composeRule.onNodeWithContentDescription(context.getString(R.string.open_filter_label))
+            .performClick()
+    }
 }

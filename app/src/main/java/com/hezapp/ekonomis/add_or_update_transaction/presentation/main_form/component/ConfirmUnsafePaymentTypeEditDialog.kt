d9185058ebcf43f86eba29fcd9dbb68d26ce9192
Hezbi Sulaiman <hezbisulaiman@gmail.com>
2025-09-09 11:10:43 +0700

    feat(ConfirmUnsafePaymentTypeEditDialog) : dialog untuk memberikan warning pada user apabila user mengedit sebuah transaksi dan mengubah tipe pembayaran dari cicilan menjadi cash

---
+++ app/src/main/java/com/hezapp/ekonomis/add_or_update_transaction/presentation/main_form/component/ConfirmUnsafePaymentTypeEditDialog.kt
@@ -0,0 +1,36 @@
+package com.hezapp.ekonomis.add_or_update_transaction.presentation.main_form.component
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.hezapp.ekonomis.R
+
+@Composable
+fun ConfirmUnsafePaymentTypeEditDialog(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(
+                onClick = onConfirm
+            ) {
+                Text(stringResource(R.string.yes_label))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                Text(stringResource(R.string.no_label))
+            }
+        },
+        text = {
+            Text(stringResource(
+                R.string.changing_payment_from_installment_to_cash_confirmation))
+        }
+    )
+}

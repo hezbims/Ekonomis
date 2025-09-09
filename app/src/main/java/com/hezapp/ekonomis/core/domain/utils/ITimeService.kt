7a2d59d77fbdf4ef5bde134d9ee4704119a4ba06
Hezbi Sulaiman <hezbisulaiman@gmail.com>
2025-09-09 10:49:24 +0700

    feat(TimeService) : method baru untuk getZoneId, getLocalDate, dan format localdate ke E, dd-MMM-yyyy"

--- app/src/main/java/com/hezapp/ekonomis/core/domain/utils/ITimeService.kt
+++ app/src/main/java/com/hezapp/ekonomis/core/domain/utils/ITimeService.kt
@@ -2,6 +2,9 @@ package com.hezapp.ekonomis.core.domain.utils
 
 import java.text.SimpleDateFormat
 import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Calendar
 import java.util.Locale
@@ -10,11 +13,20 @@ import java.util.TimeZone
 abstract class ITimeService {
     abstract fun getCalendar() : Calendar
     abstract fun getTimezone() : TimeZone
-    fun getZoneId() = getTimezone().toZoneId()
+    fun getZoneId(): ZoneId = getTimezone().toZoneId()
+    fun getLocalDate(): LocalDate = getCalendar().run {
+        LocalDate.now()
+            .withYear(get(Calendar.YEAR))
+            .withMonth(get(Calendar.MONTH) + 1)
+            .withDayOfMonth(get(Calendar.DAY_OF_MONTH))
+    }
     abstract fun getLocale() : Locale
 
     private val dayDateMonthYearFormat by lazy {
         SimpleDateFormat("E, dd-MMM-yyyy", getLocale())
+    }
+    private val dayDateMonthYearFormatV2 by lazy {
+        DateTimeFormatter.ofPattern("E, dd-MMM-yyyy", getLocale())
     }
 
     private val monthYearWordLongFormat by lazy {
@@ -33,6 +45,10 @@ abstract class ITimeService {
         dayDateMonthYearFormat.format(getCalendar().apply {
             this.timeInMillis = timeInMillis
         }.time)
+
+    fun toEddMMMyyyy(date: LocalDate): String =
+        ZonedDateTime.of(date.atStartOfDay(), getZoneId())
+            .format(dayDateMonthYearFormatV2)
 
     fun toMMMMyyyy(timeInMillis: Long): String =
         monthYearWordLongFormat.format(getCalendar().apply {

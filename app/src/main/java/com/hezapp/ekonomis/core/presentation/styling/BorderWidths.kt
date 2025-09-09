4bb931ddceb734e231fc745cbf1c7f86779ee61e
Hezbi Sulaiman <hezbisulaiman@gmail.com>
2025-09-09 10:51:32 +0700

    feat(constant styling) : konstanta baru untuk elevation dan border width

---
+++ app/src/main/java/com/hezapp/ekonomis/core/presentation/styling/BorderWidths.kt
@@ -0,0 +1,9 @@
+package com.hezapp.ekonomis.core.presentation.styling
+
+import androidx.compose.ui.unit.dp
+
+object BorderWidths {
+    val small = 0.25.dp
+    val bold = 0.75.dp
+    val normal = 1.dp
+}
---
+++ app/src/main/java/com/hezapp/ekonomis/core/presentation/styling/Elevations.kt
@@ -0,0 +1,7 @@
+package com.hezapp.ekonomis.core.presentation.styling
+
+import androidx.compose.ui.unit.dp
+
+object Elevations {
+    val normal = 0.75.dp
+}

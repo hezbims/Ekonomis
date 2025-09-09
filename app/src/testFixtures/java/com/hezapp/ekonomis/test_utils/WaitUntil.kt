fb6d0580b23b0f3baac257e0d38454c221816c56
Hezbi Sulaiman <hezbisulaiman@gmail.com>
2025-09-09 10:53:41 +0700

    refactor test utils (WaitUntil) : sekarang ada parameter tambahan untuk custom error message

--- app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/WaitUntil.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/WaitUntil.kt
@@ -1,25 +1,30 @@
 package com.hezapp.ekonomis.test_utils
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.util.Calendar
 
 fun tryUntilSucceed(
     timeout : Long = TestConstant.SMALL_TIMEOUT,
+    errorMessage: suspend () -> String? = { null },
     body: suspend () -> Unit,
 ) : Unit =  runBlocking {
-    val startTime = Calendar.getInstance().timeInMillis
-    var isSucceed = false
+    withContext(Dispatchers.IO) {
+        val startTime = Calendar.getInstance().timeInMillis
+        var isSucceed = false
 
-    while(Calendar.getInstance().timeInMillis < startTime + timeout && !isSucceed) {
-        try {
-            body()
-            isSucceed = true
-        } catch (_: Throwable) {
-            delay(50L)
+        while (Calendar.getInstance().timeInMillis < startTime + timeout && !isSucceed) {
+            try {
+                body()
+                isSucceed = true
+            } catch (_: Throwable) {
+                delay(50L)
+            }
         }
-    }
 
-    if (!isSucceed)
-        throw RuntimeException("Gagal mencoba berulang kali")
+        if (!isSucceed)
+            throw RuntimeException(errorMessage() ?: "Gagal mencoba berulang kali")
+    }
 }

4586e07b4fc81d4a8535509c39b4e3e904f8a50a
Hezbi Sulaiman <hezbisulaiman@gmail.com>
2025-09-17 14:12:52 +0700

    refactor(WaitUntil) : sekarang wait until bakal ngerecord stacktrace (sebelumnya gk record karena pindah thread terlalu premature dengan withContext)

--- app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/WaitUntil.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/WaitUntil.kt
@@ -1,30 +1,60 @@
 package com.hezapp.ekonomis.test_utils
 
+import android.os.Build
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import java.util.Calendar
 
+/**
+ * Ini untuk nunggu IO operation, bukan UI operation (karena pakai `Dispatchers.IO`.
+ * Kalau mau nunggu UI, pakai `composeRule.waitUntil`
+ */
 fun tryUntilSucceed(
     timeout : Long = TestConstant.SMALL_TIMEOUT,
     errorMessage: suspend () -> String? = { null },
     body: suspend () -> Unit,
 ) : Unit =  runBlocking {
-    withContext(Dispatchers.IO) {
-        val startTime = Calendar.getInstance().timeInMillis
+    var latestException: Throwable? = null
+    val isSucceed : Boolean = withContext(Dispatchers.IO) {
+        val isRobolectric: Boolean = Build.FINGERPRINT.contains("robolectric", true)
+        var currentTime = if (isRobolectric) 0 else Calendar.getInstance().timeInMillis
+        val endTime = currentTime + timeout
         var isSucceed = false
 
-        while (Calendar.getInstance().timeInMillis < startTime + timeout && !isSucceed) {
+        while (true) {
             try {
                 body()
                 isSucceed = true
-            } catch (_: Throwable) {
-                delay(50L)
+            } catch (t: Throwable) {
+                latestException = t
+
+                currentTime =
+                    if (isRobolectric)
+                        currentTime + 50
+                    else
+                        Calendar.getInstance().timeInMillis
+
+                if (currentTime < endTime) {
+                    if (!isRobolectric)
+                        delay(50L)
+                    continue
+                }
+                else break
             }
         }
 
-        if (!isSucceed)
-            throw RuntimeException(errorMessage() ?: "Gagal mencoba berulang kali")
+        isSucceed
     }
+
+    if (isSucceed)
+        return@runBlocking
+
+    val errorMessage = withContext(Dispatchers.IO) { errorMessage() }
+
+    if (errorMessage != null)
+        throw RuntimeException(errorMessage())
+
+    throw latestException ?: RuntimeException("Terjadi kesalahan tidak diketahui")
 }

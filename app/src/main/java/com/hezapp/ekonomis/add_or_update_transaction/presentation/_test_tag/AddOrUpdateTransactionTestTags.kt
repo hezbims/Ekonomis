ec0306f55610f24738565a22c55077c5f50181fc
hezbims <hezbisulaiman@gmail.com>
2026-04-10 08:50:35 +0700

    refactor-test (Installment List Item Test Tag) : agar bisa mengassert jumlah installment item dengan exact

---
+++ app/src/main/java/com/hezapp/ekonomis/add_or_update_transaction/presentation/_test_tag/AddOrUpdateTransactionTestTags.kt
@@ -0,0 +1,5 @@
+package com.hezapp.ekonomis.add_or_update_transaction.presentation._test_tag
+
+object AddOrUpdateTransactionTestTags {
+    const val installmentListItem = "installment-list-item"
+}
--- app/src/main/java/com/hezapp/ekonomis/add_or_update_transaction/presentation/main_form/component/InstallmentListItem.kt
+++ app/src/main/java/com/hezapp/ekonomis/add_or_update_transaction/presentation/main_form/component/InstallmentListItem.kt
@@ -4,24 +4,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.DialogProperties
 import com.hezapp.ekonomis.R
+import com.hezapp.ekonomis.add_or_update_transaction.presentation._test_tag.AddOrUpdateTransactionTestTags
 import com.hezapp.ekonomis.add_or_update_transaction.presentation.main_form.dto.InstallmentItemUiDto
 import com.hezapp.ekonomis.core.domain.invoice.entity.PaymentMedia
 import com.hezapp.ekonomis.core.domain.utils.ITimeService
@@ -74,7 +69,8 @@ fun InstallmentListItem(
                     )
                 }
             }
-        }
+        },
+        modifier = Modifier.testTag(AddOrUpdateTransactionTestTags.installmentListItem),
     )
 
     InstallmentItemBottomSheetForm(

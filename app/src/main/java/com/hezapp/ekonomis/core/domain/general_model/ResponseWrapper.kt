4529b5f5450d847feec2798f0825dc1e7a12ab88
hezbims <hezbisulaiman@gmail.com>
2026-04-25 10:21:42 +0700

    refactor(ResponseWrapper) : It is now not necessary to extend error with MyBasicError for more flexibility

--- app/src/main/java/com/hezapp/ekonomis/core/domain/general_model/ResponseWrapper.kt
+++ app/src/main/java/com/hezapp/ekonomis/core/domain/general_model/ResponseWrapper.kt
@@ -1,9 +1,9 @@
 package com.hezapp.ekonomis.core.domain.general_model
 
-sealed class ResponseWrapper<T , E : MyBasicError> {
-    class Succeed<T , E : MyBasicError>(val data : T) : ResponseWrapper<T , E>()
-    class Failed<T , E : MyBasicError>(val error : E? = null) : ResponseWrapper<T , E>()
-    class Loading<T , E : MyBasicError> : ResponseWrapper<T, E>()
+sealed class ResponseWrapper<T , E> {
+    class Succeed<T , E>(val data : T) : ResponseWrapper<T , E>()
+    class Failed<T , E>(val error : E? = null) : ResponseWrapper<T , E>()
+    class Loading<T , E> : ResponseWrapper<T, E>()
 
     fun isSucceed() : Boolean = this is Succeed
     fun isFailed() : Boolean = this is Failed

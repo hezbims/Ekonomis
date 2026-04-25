dcf671293fa55f15cb396905ef02aaeec3f1df95
hezbims <hezbisulaiman@gmail.com>
2026-04-25 11:21:51 +0700

    refactor(BaseUnitTest) : separate between data and ui unit test for more efficient run

---
+++ app/src/test/java/com/hezapp/ekonomis/test_application/BaseDataUnitTest.kt
@@ -0,0 +1,59 @@
+package com.hezapp.ekonomis.test_application
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.hezapp.ekonomis.MainApplication
+import com.hezapp.ekonomis.test_utils.TestDataUtils
+import com.hezapp.ekonomis.test_utils.gherkin.IGherkinSyntax
+import com.hezapp.ekonomis.test_utils.rule.GlobalTimeConfigRule
+import com.hezapp.ekonomis.test_utils.rule.TestEnvironmentResetRule
+import org.junit.Rule
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.Koin
+import org.koin.core.KoinApplication
+import org.koin.dsl.koinApplication
+import org.robolectric.shadows.ShadowLog
+
+@RunWith(AndroidJUnit4::class)
+abstract class BaseDataUnitTest : IGherkinSyntax {
+    private var _koinApp: KoinApplication? = null
+    protected val koinApp: KoinApplication
+        get() = _koinApp!!
+    protected val koin: Koin
+        get() = _koinApp!!.koin
+    protected val appContext : Context = ApplicationProvider.getApplicationContext()
+
+    @get:Rule(order = 0)
+    val koinAppRule = object : TestWatcher(){
+        override fun starting(description: Description?) {
+            ShadowLog.stream = System.out
+            _koinApp = koinApplication {
+                allowOverride(true)
+                androidContext(appContext)
+                modules(MainApplication.koinModules)
+            }.apply {
+                loadTestKoinModules(appContext = appContext, koin = koin, useInMemoryDb = true)
+            }
+            super.starting(description)
+        }
+
+        override fun finished(description: Description?) {
+            super.finished(description)
+            _koinApp?.close()
+        }
+    }
+
+    @get:Rule(1)
+    val globalTimeConfigRule = GlobalTimeConfigRule()
+
+    @get:Rule(2)
+    val testEnvironementResetRule = TestEnvironmentResetRule { koin }
+
+    val dataUtils by lazy {
+        TestDataUtils(koin = koin)
+    }
+}

759f2b44fbd1ec715b1a01246328545da8a4cdee
hezbims <hezbisulaiman@gmail.com>
2026-05-04 09:47:27 +0700

    fix(TestEnvironmentResetRule) : close DB agar tidak menimbulkan log error

--- app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/rule/TestEnvironmentResetRule.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/rule/TestEnvironmentResetRule.kt
@@ -4,6 +4,7 @@ import com.hezapp.ekonomis.core.data.database.EkonomisDatabase
 import com.hezapp.ekonomis.test_utils.TestTimeService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withContext
 import org.junit.rules.ExternalResource
 import org.koin.core.Koin
@@ -19,5 +20,11 @@ class TestEnvironmentResetRule(
             getKoin().getOrNull<EkonomisDatabase>()?.clearAllTables()
         }
         TestTimeService.reset()
+    }
+
+    override fun after() = runTest {
+        withContext(Dispatchers.IO){
+            getKoin().getOrNull<EkonomisDatabase>()?.close()
+        }
     }
 }

2989b530db91b8650e6b81363ffd88cc4b18f840
hezbims <hezbisulaiman@gmail.com>
2026-06-07 14:00:15 +0700

    refactor-test(ProfileSeeder) : new method to not return direct room entity

--- app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/seeder/ProfileSeeder.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/seeder/ProfileSeeder.kt
@@ -3,6 +3,7 @@ package com.hezapp.ekonomis.test_utils.seeder
 import com.hezapp.ekonomis.core.data.profile.dao.ProfileDao
 import com.hezapp.ekonomis.core.domain.profile.entity.ProfileEntity
 import com.hezapp.ekonomis.core.domain.profile.entity.ProfileType
+import com.hezapp.ekonomis.test_utils.seeder.snapshot.ProfileSnapshot
 import org.koin.core.Koin
 import org.koin.core.context.GlobalContext
 
@@ -10,6 +11,20 @@ class ProfileSeeder(
     koin: Koin = GlobalContext.get(),
 ) {
     private val profileDao: ProfileDao = koin.get()
+
+    suspend fun runV2(profileName: String, profileType: ProfileType) : ProfileSnapshot {
+        val ids = profileDao.insertProfiles(listOf(
+            ProfileEntity(name = profileName, type = profileType)
+        ))
+        return profileDao.getProfilesByIds(ids.map(Long::toInt))
+            .map(ProfileSnapshot::fromRoomEntity)
+            .single()
+    }
+
+    @Deprecated(
+        message = "Replace with runV2 so it don't immediately return ProfileEntity",
+        replaceWith = ReplaceWith("runV2(profileName, profileType)")
+    )
     suspend fun run(profileName: String, profileType: ProfileType) : ProfileEntity {
         val ids = profileDao.insertProfiles(listOf(
             ProfileEntity(name = profileName, type = profileType)
--- app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/seeder/snapshot/ProfileSnapshot.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/seeder/snapshot/ProfileSnapshot.kt
@@ -1,9 +1,20 @@
 package com.hezapp.ekonomis.test_utils.seeder.snapshot
 
+import com.hezapp.ekonomis.core.domain.profile.entity.ProfileEntity
 import com.hezapp.ekonomis.core.domain.profile.entity.ProfileType
 
 data class ProfileSnapshot(
     val id: Int,
     val name: String,
     val type: ProfileType,
-)
+){
+    companion object {
+        fun fromRoomEntity(profileEntity: ProfileEntity) : ProfileSnapshot {
+            return ProfileSnapshot(
+                id = profileEntity.id,
+                name = profileEntity.name,
+                type = profileEntity.type,
+            )
+        }
+    }
+}

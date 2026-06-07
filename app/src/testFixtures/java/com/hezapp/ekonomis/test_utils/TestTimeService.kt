2241ac7b661876a7678498c63383aa888b33fbf7
hezbims <hezbisulaiman@gmail.com>
2026-06-07 16:01:17 +0700

    refactor-test(TestTimeService) : new method for set current time and better design to avoiding singleton

--- app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/TestTimeService.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_utils/TestTimeService.kt
@@ -1,21 +1,34 @@
 package com.hezapp.ekonomis.test_utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.hezapp.ekonomis.core.domain.utils.ITimeService
 import com.hezapp.ekonomis.core.domain.utils.TimeService
+import java.time.LocalDate
+import java.time.ZoneId
 import java.util.Calendar
 import java.util.Locale
 import java.util.TimeZone
 
 class TestTimeService(
-    private val currentTimeInMillis: Long? = null,
-    private val timeZone: TimeZone? = null,
-    private val locale: Locale? = null,
+    private var currentTimeInMillis: Long = defaultTestCalendar.timeInMillis,
+    private var timeZone: TimeZone = defaultTestTimezone.clone() as TimeZone,
+    private val locale: Locale = defaultTestLocale.clone() as Locale,
 ) : ITimeService()  {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setCurrentTime(localDate: LocalDate, zoneId: ZoneId? = null){
+        if (zoneId != null)
+            timeZone = TimeZone.getTimeZone(zoneId)
+
+        currentTimeInMillis = localDate
+            .atStartOfDay(getTimezone().toZoneId())
+            .toInstant()
+            .toEpochMilli()
+    }
+
     companion object {
         private val productionTimeService = TimeService()
-        private val instance = TestTimeService()
-        fun get() = instance
-
         private val defaultTestTimezone = productionTimeService.getTimezone()
         private val defaultTestCalendar = productionTimeService.getCalendar().apply {
             set(
@@ -27,33 +40,43 @@ class TestTimeService(
                 0
             )
         }
+        private val defaultTestLocale = productionTimeService.getLocale()
 
-        /**
-         * Silahkan ubah ini sesuai kebutuhan
-         */
+        private val instance = TestTimeService()
+        @Deprecated(message = "Avoid using this singleton method. " +
+                "Please create new TestTimeService instead for every test, " +
+                "and inject it using koin for better isolation")
+        fun get() = instance
+
         private var testTimeZone: TimeZone =  defaultTestTimezone
         @Suppress("unused")
+        @Deprecated(message = "Avoid using this singleton method. " +
+                "Please create new TestTimeService instead and use setCurrentTime method")
         fun setTestTimeZone(newTimeZone: TimeZone){
             testTimeZone = newTimeZone
         }
 
-        /**
-         * Silahkan ubah ini sesuai kebutuhan test
-         */
         private var testCalendar: Calendar = defaultTestCalendar
         @Suppress("unused")
+        @Deprecated(message = "Avoid using this singleton method. " +
+                "Please create new TestTimeService instead and use setCurrentTime method")
         fun setTestCalendar(newCalendar: Calendar){
             testCalendar = newCalendar
         }
 
+        @Deprecated(message = "Avoid using this singleton method. " +
+                "Please create new TestTimeService instead for every test, " +
+                "and inject it using koin for better isolation")
         fun reset(){
             testTimeZone = defaultTestTimezone
             testCalendar = defaultTestCalendar
         }
     }
-    override fun getCurrentTimeInMillis(): Long = currentTimeInMillis ?: testCalendar.timeInMillis
-    override fun getTimezone(): TimeZone = timeZone ?: testTimeZone.clone() as TimeZone
-    override fun getLocale(): Locale = locale ?: productionTimeService.getLocale()
+    override fun getCurrentTimeInMillis(): Long = currentTimeInMillis
+    override fun getTimezone(): TimeZone = timeZone
+    override fun getLocale(): Locale = locale
 }
 
+@Deprecated("Avoid this singleton. " +
+        "Please create new TestTimeService for every test instead for better test isolation")
 val testCalendarProvider = TestTimeService.get()

d156975141dc7fecde08f8c5c443e0a05c4a6022
hezbims <hezbisulaiman@gmail.com>
2026-06-10 09:42:53 +0700

    refactor(TestKoinModule) : enable TestTimeService to be resolved

--- app/src/testFixtures/java/com/hezapp/ekonomis/test_application/TestKoinModule.kt
+++ app/src/testFixtures/java/com/hezapp/ekonomis/test_application/TestKoinModule.kt
@@ -6,10 +6,10 @@ import androidx.annotation.RequiresApi
 import androidx.room.Room
 import androidx.room.RoomDatabase.Callback
 import androidx.sqlite.db.SupportSQLiteDatabase
-import com.hezapp.ekonomis.core.data.database.EkonomisDatabase
-import com.hezapp.ekonomis.core.domain.utils.ITimeService
 import com.hezapp.ekonomis._testing_only.EkonomisTestDatabase
+import com.hezapp.ekonomis.core.data.database.EkonomisDatabase
 import com.hezapp.ekonomis.core.domain.utils.IErrorReportingService
+import com.hezapp.ekonomis.core.domain.utils.ITimeService
 import com.hezapp.ekonomis.test_utils.FakeErrorReportingService
 import com.hezapp.ekonomis.test_utils.TestTimeService
 import org.koin.core.Koin
@@ -48,7 +48,7 @@ fun loadTestKoinModules(
         single { testDb.installmentTestDao }
         single { testDb.installmentItemTestDao }
         //endregion
-        single<ITimeService> {
+        single<TestTimeService> {
             val timeZone = TimeZone.getTimeZone("GMT+8")
             TestTimeService(
                 currentTimeInMillis = ZonedDateTime.of(
@@ -59,6 +59,7 @@ fun loadTestKoinModules(
                 locale = Locale.forLanguageTag("id-ID"),
             )
         }
+        single<ITimeService> { get<TestTimeService>() }
         single<IErrorReportingService> { FakeErrorReportingService() }
     }
     koin.loadModules(listOf(module), allowOverride = true)
